66ef4fac812504b8564fd1d3200bef7b7f31e5d2
AvogatoWizardWhisker <babis_abdul@hotmail.fr>
2024-07-04 21:00:25 +0100

    - Added Newtonsoft.Json NuGet package to output operations in JSON and removed .NET Trace class.

--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -1,49 +1,79 @@
 ﻿using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace CalculatorLibrary
 {
     public class Calculator
     {
+        JsonWriter writer;
         public Calculator()
         {
-           StreamWriter logFile = File.CreateText("calculator.log");
-           Trace.Listeners.Add(new TextWriterTraceListener(logFile));
-           Trace.AutoFlush = true;
-           Trace.WriteLine("Starting Calculator Log");
-           Trace.WriteLine(String.Format("Starting {0}", System.DateTime.Now.ToString()));
+            //StreamWriter logFile = File.CreateText("calculator.log");
+            //Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+            //Trace.AutoFlush = true;
+            //Trace.WriteLine("Starting Calculator Log");
+            //Trace.WriteLine(String.Format("Starting {0}", System.DateTime.Now.ToString()));
+
+            StreamWriter logfile = File.CreateText("calculator.json");
+            logfile.AutoFlush = true;
+            writer = new JsonTextWriter(logfile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
         }
         public double DoOperation(double num1, double num2, string op)
         {
             double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
             // Use a switch statement to do the math.
             switch (op)
             {
                 case "+":
                     result = num1 + num2;
-                    Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    //Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    writer.WriteValue("+");
+
                     break;
                 case "-":
                     result = num1 - num2;
-                    Trace.WriteLine(String.Format("{0} - {1} = {2}", num1, num2, result));
+                    //Trace.WriteLine(String.Format("{0} - {1} = {2}", num1, num2, result));
+                    writer.WriteValue("-");
                     break;
                 case "*":
                     result = num1 * num2;
-                    Trace.WriteLine(String.Format("{0} * {1} = {2}", num1, num2, result));
+                    //Trace.WriteLine(String.Format("{0} * {1} = {2}", num1, num2, result));
+                    writer.WriteValue("*");
                     break;
                 case "/":
                     // Ask the user to enter a non-zero divisor.
                     if (num2 != 0)
                     {
                         result = num1 / num2;
-                        Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
+                        //Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
                     }
+                    writer.WriteValue("/");
                     break;
                 // Return text for an incorrect option entry.
                 default:
                     break;
             }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
             return result;
+        }
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
         }
     }
 
